665566bd2f3dbe20af89b174a6247251a3fc60a0
Koh Zhi Kai <highspeedlowdrag_89@hotmail.com>
2012-03-03 16:23:03 +0800

    removed comment

--- src/GUI.java
+++ src/GUI.java
@@ -69,7 +69,7 @@ public class GUI extends JFrame {
 	private JTextField textField2_Search;
 	private JTable table3;
 	
-	public GUI() { // GUI
+	public GUI() {
     	
         setTitle("Manage It Right! v0.1");
         final JTabbedPane jtp = new JTabbedPane();
