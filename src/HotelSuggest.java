e649223215b15afea3daac486c9d664dd325d088
Poh <pohchiat@gmail.com>
2012-03-03 15:56:43 +0800

    HotelSuggest.java

---
+++ src/HotelSuggest.java
@@ -0,0 +1,163 @@
+import java.io.File;
+import java.util.*;
+
+public class HotelSuggest {
+	
+	private Vector<Hotel> hotelList = null;
+	private int stars;
+	private double eventBudget;
+	private double budgetRatio;
+	private Calendar startDate;
+	private MealType eventMealType;
+	private int numberOfGuests; //assume tables of 10pax each
+	
+	//HotelSuggest constructor
+	public HotelSuggest(Vector<Hotel> hotelList, int stars, double eventBudget,	double budgetRatio, Calendar startDate, MealType eventMealType, int numberOfGuests){
+		this.hotelList = hotelList;
+		this.stars = stars;
+		this.eventBudget = eventBudget;
+		this.budgetRatio = budgetRatio;
+		this.startDate = startDate;
+		this.eventMealType = eventMealType;
+		this.numberOfGuests = numberOfGuests;
+	}
+	
+	public HotelSuggest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	//HotelSuggest Getters and Setters
+	public Vector<Hotel> getHotelList() {
+		return hotelList;
+	}
+
+	public void setHotelList(Vector<Hotel> hotelList) {
+		this.hotelList = hotelList;
+	}
+
+	public int getStars() {
+		return stars;
+	}
+
+	public void setStars(int stars) {
+		this.stars = stars;
+	}
+
+	public double getEventBudget() {
+		return eventBudget;
+	}
+
+	public void setEventBudget(double eventBudget) {
+		this.eventBudget = eventBudget;
+	}
+
+	public double getBudgetRatio() {
+		return budgetRatio;
+	}
+
+	public void setBudgetRatio(double budgetRatio) {
+		this.budgetRatio = budgetRatio;
+	}
+
+	public Calendar getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Calendar startDate) {
+		this.startDate = startDate;
+	}
+
+	public MealType getEventMealType() {
+		return eventMealType;
+	}
+
+	public void setEventMealType(MealType eventMealType) {
+		this.eventMealType = eventMealType;
+	}
+
+	public int getNumberOfGuests() {
+		return numberOfGuests;
+	}
+
+	public void setNumberOfGuests(int numberOfGuests) {
+		this.numberOfGuests = numberOfGuests;
+	}
+	
+	private DayType getDayType(Calendar startDate){
+		int day = startDate.get(Calendar.DAY_OF_WEEK); 
+		DayType daytype = DayType.NORMAL;
+		
+		switch(day){
+			case 1:
+			case 6:
+			case 7:
+				daytype =  DayType.FRI_SUN; //FRI THRU TO SUNDAY
+				break;
+			case 2:							//MONDAY 
+			case 3:							//THRU 
+			case 4:							//TO
+			case 5: 						//THURSDAY
+				daytype = DayType.NORMAL;
+				break;
+		}
+
+		return daytype;
+	}
+
+	public Vector<Hotel> suggest(DataManager dataM){
+		//Get day of week from start date of event
+		DayType eventDayType = getDayType(startDate);
+		
+		
+		//Read hotelList from DataManager for the first time only
+		if(hotelList == null){
+			String path = System.getProperty("user.dir");
+			File hotelFile = new File(path + File.separator + "Data" + File.separator + "hotelData");
+			this.setHotelList(dataM.loadHotels(hotelFile));
+		}
+		
+		//Calculate hotel budget
+		double hotelBudget = (eventBudget * budgetRatio/100.0);
+		
+		//Calculate number of tables required 
+		int numberOfTables;
+		if(numberOfGuests%10 != 0)
+			numberOfTables = numberOfGuests/10 + 1;
+		else 
+			numberOfTables = numberOfGuests/10;
+		
+		//Generate suggested hotels
+		Vector<Hotel> suggestedHotels = new Vector<Hotel>();
+		Hotel tryHotel;
+		Vector<Menu> tryMenu;
+		Vector<Menu> selectedMenu = new Vector<Menu>();
+		
+		Menu currentMenu;
+	
+		for (int i=0; i<hotelList.size(); ++i){ //scan through hotel list
+			
+			if (hotelList.get(i).getStars() == stars){ //if hotel matches preferred star rating
+				tryHotel = hotelList.get(i);
+				tryMenu = tryHotel.getMenuList();
+
+				for (int j=0; j<tryMenu.size(); ++j){ //scan through menu list of current hotel
+					currentMenu = tryMenu.get(j);
+					if (currentMenu.getDayType() == eventDayType && currentMenu.getMealType() == eventMealType){ //if required day and required meal types match offerings
+						
+						if (currentMenu.getPricePerTable()*numberOfTables <= hotelBudget)	//if menu falls within hotel budget
+							selectedMenu.add(currentMenu); 								 	//add to selected menu list
+						
+					}
+				}
+			
+				tryHotel.setMenuList(selectedMenu); 
+				
+				if (!tryHotel.getMenuList().isEmpty())	//if hotel possesses at least one affordable menu item
+					suggestedHotels.add(tryHotel);		//add hotel to suggested list
+			}
+		}
+		
+		return suggestedHotels;
+	}
+	
+}
