8440f8e50bf8b1f41b9e54c6de4ccb9a76446845
Eugene Sim <seugienem@gmail.com>
2012-03-25 00:59:23 +0800

    Added Expense class

---
+++ src/Expense.java
@@ -0,0 +1,47 @@
+
+public class Expense {
+	private String itemName;
+	private double unitCost;
+	private int quantity;
+	private double totalCost;
+	
+	public Expense(String itemName, double unitCost, int quantity,double totalCost) {
+		this.itemName = "";
+		this.unitCost = 0.0;
+		this.quantity = 0;
+		this.totalCost = 0.0;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public double getUnitCost() {
+		return unitCost;
+	}
+
+	public void setUnitCost(double unitCost) {
+		this.unitCost = unitCost;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+	
+}
