7f1e79d10d86f6ecea3f2d9a00d451a4c457ead3
Koh Zhi Kai <highspeedlowdrag_89@hotmail.com>
2012-04-08 16:18:11 +0800

    Added new logo. Added Logo and Welcome text to Overview Tab.

--- src/GUI.java
+++ src/GUI.java
@@ -33,6 +33,8 @@ public class GUI extends JFrame implements FocusListener, MouseListener {
 	private File currEventFileDirectory;
 	private JTabbedPane jtp;
 	
+	private ImageIcon logo;
+	
 	//Menu Bar objects
 	private JMenu mnFile;
 	private JMenuBar menuBar;
@@ -152,6 +154,11 @@ public class GUI extends JFrame implements FocusListener, MouseListener {
         jtp.setFont(new Font("Segoe UI", Font.PLAIN, 12));
         jtp.setTabPlacement(JTabbedPane.LEFT);
         getContentPane().add(jtp, BorderLayout.CENTER);
+        
+        try {
+        	logo = new ImageIcon("Data/MIRlogo.png");
+            setIconImage(logo.getImage());
+        } catch (Exception e) {} // does not load logo if logo image is not found.
               
         try {
      		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
@@ -353,12 +360,11 @@ public class GUI extends JFrame implements FocusListener, MouseListener {
 		lbl0_Overview.setBounds(10, 11, 76, 30);
 		lbl0_Overview.setFont(new Font("Tahoma", Font.BOLD, 16));
 		
-		if (this.getClass().getResource("MIR_logo.jpg") != null) {
-			ImageIcon myPicture = new ImageIcon(this.getClass().getResource("MIR_logo.jpg"));
-			JLabel lbl0_Logo = new JLabel(myPicture);
-	        lbl0_Logo.setBounds(351, 52, 300, 115);
+		try{
+			JLabel lbl0_Logo = new JLabel(logo);
+	        lbl0_Logo.setBounds(350, 66, 128, 128);
 	        panel0.add(lbl0_Logo);
-		}
+		} catch (Exception e) {}
 		
 				
 		 btn0_New = new JButton("New");        
@@ -414,10 +420,20 @@ public class GUI extends JFrame implements FocusListener, MouseListener {
 		 panel0.add(lbl0_Overview);
         
         textPane0_EventName = new JTextPane();
+        textPane0_EventName.setOpaque(false);
         textPane0_EventName.setEditable(false);
         textPane0_EventName.setFont(new Font("Tahoma", Font.PLAIN, 16));
         textPane0_EventName.setBounds(136, 11, 504, 30);
         panel0.add(textPane0_EventName);
+        
+        JTextArea textArea0_Welcome = new JTextArea();
+        textArea0_Welcome.setOpaque(false);
+        textArea0_Welcome.setEditable(false);
+        textArea0_Welcome.setAutoscrolls(false);
+        textArea0_Welcome.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        textArea0_Welcome.setText("Welcome to\nManage It Right! v0.2\n\nStart by creating a new\nevent or loading a\npreviously saved event.\n\nFollow the steps listed\non the tabs by the left.");
+        textArea0_Welcome.setBounds(500, 66, 140, 139);
+        panel0.add(textArea0_Welcome);
         
         lbl0_Step1 = new JLabel();
         lbl0_Step1.setText("  Event Details are empty.");
@@ -469,7 +485,7 @@ public class GUI extends JFrame implements FocusListener, MouseListener {
         panel0.add(lbl0_Step6);
         
         panel0.add(btn0_New);
-        panel0.add(btn0_Load);   
+        panel0.add(btn0_Load);
 	}
 	
 	/*
@@ -2976,6 +2992,4 @@ public class GUI extends JFrame implements FocusListener, MouseListener {
 	    }
 
 	}
-
-	
 }
