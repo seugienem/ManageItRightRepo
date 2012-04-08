e438646d5e89621906c297207e66026b450445d7
poh chiat <pohchiat@gmail.com>
2012-04-08 23:10:38 +0800

    Clean up progress event and progress listener.

--- src/ProgressEvent.java
+++ src/ProgressEvent.java
@@ -1,6 +1,17 @@
+/*
+ * ProgressEvent.java - A customized event used by TableAssigner.java.
+ * Authors: Team MIR
+ * 
+ * 
+ * Description: This inherits the generic event object. It helps
+ * TableAssigner to fire whenever there's progress in during the running
+ * of the algorithm.
+ */
+
 import java.util.EventObject;
 
 public class ProgressEvent extends EventObject{
+	private static final long serialVersionUID = 1L;
 	private int progressValue;
 	
 	public ProgressEvent(Object source, int progressValue){
--- src/ProgressListener.java
+++ src/ProgressListener.java
@@ -1,6 +1,14 @@
+/*
+ * ProgressListener.java - A customized listener for progress events.
+ * Authors: Team MIR
+ * 
+ * 
+ * Description: This inherits the generic event listener and is customized
+ * to generate Progress Events. (See ProgressEvent.java)
+ */
+
 import java.util.EventListener;
 
 public interface ProgressListener extends EventListener{
-
 	public void progressEventOccured(ProgressEvent evt);
 }
